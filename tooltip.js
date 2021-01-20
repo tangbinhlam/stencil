2744bab09f2acc7b350ad897af74e293c1046465
osalam <tangbinhlam@gmail.com>
2021-01-20 11:01:04 -0800

    Use connectedCallback for add data to Tooltip with unserstand costom element lifecycle and add even moveeter and mouseleave to controle tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,25 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        console.log('Custom Element is working');
+        this._tooltipContainer;
+    }
+
+    connectedCallback() {
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip);
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
+        this.append(tooltipIcon);
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = "This is tooltip text!";
+        this.append(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this._tooltipContainer.remove();
     }
 }
 
