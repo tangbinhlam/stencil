cd86f97409a89a6247773631924f7732a0d6ea29
osalam <tangbinhlam@gmail.com>
2021-01-20 11:54:32 -0800

    Stylling our Element

--- tooltip.js
+++ tooltip.js
@@ -11,11 +11,18 @@ class Tooltip extends HTMLElement {
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
         this.append(tooltipIcon);
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
+        // Set Content
         this._tooltipContainer.textContent = this._tooltipText;
+        // Set style
+        this._tooltipContainer.style.background = 'grey';
+        this._tooltipContainer.style.color = 'white';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
         this.append(this._tooltipContainer);
     }
 
