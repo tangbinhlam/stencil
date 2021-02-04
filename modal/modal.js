b6b66b781088f0ce450b0d12814a075adf926903
osalam <tangbinhlam@gmail.com>
2021-02-03 16:01:00 -0800

    Adding the model contianer and styling the Model elements

---
+++ modal/modal.js
@@ -0,0 +1,68 @@
+customElements.define('uc-modal', class Modal extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                #backdrop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100vh;
+                    background: rgba(0,0,0,0.75);
+                    z-index: 10;
+                }
+                #modal {
+                    position: fixed;
+                    top: 15vh;
+                    left: 25%;
+                    width: 50%;
+                    z-index: 100;
+                    background: white;
+                    border-radius: 3px;
+                    box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                }
+
+                header {
+                    padding: 1rem;
+                }
+
+                header h1{
+                    front-size: 1.25rem;
+                }
+
+                #actions {
+                    border-top: 1px solid #ccc;
+                    padding: 1rem;
+                    display: flex;
+                    justify-content: flex-end;
+                }
+
+                #actions button {
+                    margin: 0 0.25rem;
+                }
+
+                #main {
+                    padding: 1rem;
+                }
+            </style>
+            <div id="backdrop"></div>
+            <div id="modal">
+                <header>
+                    <h1>Please confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>OK</button>
+                </section>
+            </div>
+        `
+    }
+})
