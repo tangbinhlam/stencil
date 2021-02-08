f57ec246c4f4a062108fc36b8719b02f3ba335cf
osalam <tangbinhlam@gmail.com>
2021-02-08 10:53:28 -0800

    Props combination with attributes

--- stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -8,15 +8,23 @@ import { Component, h, Prop } from '@stencil/core';
 export class SideDrawer {
 
   @Prop({ reflect: true }) title: string = 'My Title';
+  @Prop({ reflect: true, mutable: true }) open: boolean;
 
   render() {
     return (
       <aside>
-        <header><h1>{this.title}</h1></header>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
         <main>
           <slot></slot>
         </main>
       </aside>
     );
+  }
+
+  onCloseDrawer() {
+    this.open = false;
   }
 }
