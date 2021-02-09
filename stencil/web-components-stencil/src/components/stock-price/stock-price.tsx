2bfdcc22dc8bf2b819bb4142aaf5f344f286e9e8
osalam <tangbinhlam@gmail.com>
2021-02-08 16:32:19 -0800

    Using references

--- stencil/web-components-stencil/src/components/stock-price/stock-price.tsx
+++ stencil/web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -10,9 +10,12 @@ export class StockPrice {
   @Element() el: HTMLElement;
   @State() price: number;
 
+  symbolInput: HTMLInputElement;
+
   onFetchStockPrice(event: Event) {
     event.preventDefault();
-    const symbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    //const symbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const symbol = this.symbolInput.value;
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=demo`)
       .then(res => { return res.json(); })
       .then(parsedRes => {
@@ -25,7 +28,7 @@ export class StockPrice {
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol"></input>
+        <input id="stock-symbol" ref={el => this.symbolInput = el}></input>
         <button type="submit">Fetch</button>
       </form>,
       <div>
